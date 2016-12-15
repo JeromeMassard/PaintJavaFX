30b90921b03a1dfea263ad42c243f337c5f868da
bilombardy <bilombardy@b20-73.dpt_info.iut.local>
2016-12-15 09:30:55 +0100

    Ajout du databinding des dimensions de l'image et du cadre du Calque dans Layer.java Ajout des images "edit_true" et "edit_false" permettant d'informer l'utilisateur de la possibilité de modifier le nom du Calque

--- Paint/src/paint/Layer.java
+++ Paint/src/paint/Layer.java
@@ -1,26 +1,22 @@
 package paint;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 import javafx.beans.property.*;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableArray;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import javafx.scene.input.*;
 import javafx.scene.layout.*;
 
-public class Layer extends BorderPane {
-
-private ObjectProperty<Image> image = new SimpleObjectProperty<>();
-    
-    public static ObservableValue<Double> rootHeight;
-    public static ObservableValue<Double> rootWidth;
-
-    private boolean active = false;
+public class Layer extends BorderPane implements Initializable {
+    public static SimpleDoubleProperty rootHeight = new SimpleDoubleProperty(100);
+    public static SimpleDoubleProperty rootWidth = new SimpleDoubleProperty(200);
     
     @FXML
     private ImageView imageView;
+    private ObjectProperty<Image> image = new SimpleObjectProperty<>();
     
     @FXML
     private CheckBox visible;
@@ -33,34 +29,57 @@ private ObjectProperty<Image> image = new SimpleObjectProperty<>();
     
     @FXML
     private Button edit;
+    /** Is edit Button active*/
+    private boolean active = false;
         
-    public Layer() {
+    public Layer() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/layer.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
-
-        try {
-            fxmlLoader.load();
-        } catch (IOException exception) {
-            throw new RuntimeException(exception);
-        }
+        fxmlLoader.load();
+        
+        this.prefHeightProperty().addListener((obs, oldVal, newVal) -> this.setHeight(rootHeight.getValue()));
+        this.prefWidthProperty().addListener((obs, oldVal, newVal) -> this.setHeight(rootWidth.getValue()));
+        
+        imageView.fitHeightProperty().bind(rootHeight);
+        imageView.fitWidthProperty().bind(rootWidth);
     }
     
-    /* ===== ACTIONS ===== */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        edit.setBackground(new Background(Images.EDIT_FALSE));
+    }
+    
+    /**
+     * Event called when pushing edit button
+     */
+    @FXML
     public void editName() {
         active = !active;
         name.setEditable(active);
+        name.setDisable(!active);
         
-        if (!active) {
-            
+        if (active)
+            edit.setBackground(new Background(Images.EDIT_TRUE));
+        else {
+            edit.setBackground(new Background(Images.EDIT_FALSE));
+            System.out.println("Changed name for " + toString() + " to " + getName());
         }
     }
     
-    
-    /* ===== PROPERTIES ===== */
     /**
-     * 
-     * @return 
+     * Event called when textarea field is changed
+     * @param key 
+     */
+    @FXML
+    public void editText(KeyEvent key) {
+        if (key.getCode() == KeyCode.ENTER)
+            editName();
+    }
+    
+    /**
+     * Get the Property of the Layer's image imageView
+     * @return ObjectProperty Image
      */
     public ObjectProperty<Image> imageProperty() {
         return imageView.imageProperty();
@@ -89,18 +108,17 @@ private ObjectProperty<Image> image = new SimpleObjectProperty<>();
     public StringProperty nameProperty() {
         return name.textProperty();
     }
-        
-    /* ===== GETTERS ===== */
+    
     /**
-     * 
-     * @return 
+     * Get image of the Layer
+     * @return the layer's image
      */
     public Image getImage() {
         return imageProperty().get();
     }
     
     /**
-     * Get visibility the Layer
+     * Get visibility of the Layer
      * @return true if the Layer is visible <br /> false if not
      */
     public boolean getVisibility() {
@@ -122,14 +140,13 @@ private ObjectProperty<Image> image = new SimpleObjectProperty<>();
     public String getName() {
         return nameProperty().get();
     }
-
-    /* ===== SETTERS ===== */
+    
     /**
      * Set the path for the imageview
      * @param img
      * @throws java.net.URISyntaxException 
      */
-    public void setImage(Image img) throws URISyntaxException {
+    public void setImage(Image img) throws URISyntaxException {        
         imageProperty().set(img);
     }
 
@@ -156,4 +173,16 @@ private ObjectProperty<Image> image = new SimpleObjectProperty<>();
     public void setName(String name) {
         nameProperty().set(name);
     }
+}
+
+class Images {
+    static final BackgroundImage EDIT_TRUE =  new BackgroundImage(new Image("images/edit_true.png"),  BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+    static final BackgroundImage EDIT_FALSE = new BackgroundImage(new Image("images/edit_false.png"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+/*
+    static final BackgroundImage VISIBLE_TRUE =  new BackgroundImage(new Image("images/visible_true.png"),  BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+    static final BackgroundImage VISIBLE_FALSE = new BackgroundImage(new Image("images/visible_false.png"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+
+    static final BackgroundImage LOCKED_TRUE =  new BackgroundImage(new Image("images/locked_true.png"),  BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+    static final BackgroundImage LOCKED_FALSE = new BackgroundImage(new Image("images/locked_false.png"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(75, 75, false, false, true, false));
+*/
 }

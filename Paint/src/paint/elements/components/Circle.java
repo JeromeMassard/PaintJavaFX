3cafdd2f09eed667ede6842a2f4ac16111c79d8b
bilombardy <bilombardy@b20-73.dpt_info.iut.local>
2017-01-04 11:38:28 +0100

    Ajout de composants de dessin

---
+++ Paint/src/paint/elements/components/Circle.java
@@ -0,0 +1,29 @@
+package paint.elements.components;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author bilombardy
+ */
+public class Circle extends Component {
+    private int radius;
+
+    public Circle(int posX, int posY, int radius) {
+        super(posX, posY);
+        setRadius(radius);
+    }
+    
+    private void setRadius(int radius) {
+        if (radius < 1)
+            radius = 1;
+        
+        this.radius = radius;
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.drawOval(x, y, radius, radius);
+    }
+}
---
+++ Paint/src/paint/elements/components/Oval.java
@@ -0,0 +1,39 @@
+package paint.elements.components;
+
+import java.awt.Graphics;
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ *
+ * @author bilombardy
+ */
+public class Oval extends Component {
+    private int width;
+    private int height;
+
+    public Oval(int posX, int posY, int width, int height) {
+        super(posX, posY);
+        setWidth(width);
+        setHeight(height);
+    }
+    
+    private void setWidth(int width) {
+        if (width < 1)
+            width = 1;
+        
+        this.width = width;
+    }
+    
+    private void setHeight(int height) {
+        if (height < 1)
+            height = 1;
+        
+        this.height = height;
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.drawOval(x, y, width, height);
+    }
+}

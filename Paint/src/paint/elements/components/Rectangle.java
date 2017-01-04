cc13d3d82c8ff4786257be2e18fe971adc3f527c
bilombardy <bilombardy@b20-73.dpt_info.iut.local>
2017-01-04 11:42:43 +0100

    Change Square to Rectangle

---
+++ Paint/src/paint/elements/components/Rectangle.java
@@ -0,0 +1,38 @@
+package paint.elements.components;
+
+import java.awt.Graphics;
+
+/**
+ * 
+ * @author bilombardy
+ */
+public class Rectangle extends Component {
+    private int width;
+    private int height;
+    
+    public Rectangle(int posX, int posY, int width, int height) {
+        super(posX, posY);
+        setWidth(width);
+        setHeight(height);
+    }
+    
+    private void setWidth(int width) {
+        if (width < 1)
+            width = 1;
+        
+        this.width = width;
+    }
+    
+    private void setHeight(int height) {
+        if (height < 1)
+            height = 1;
+        
+        this.height = height;
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(color);
+        g.fillRect(x, y, width, height);
+    }
+}

eb2fe1cde7d9cb4f472c3b790ef947def358d40b
bilombardy <bilombardy@b20-73.dpt_info.iut.local>
2017-01-05 09:08:37 +0100

    Ajout de la possibilité de supprimer des éléments de Group Utilisation de la fonction forEach pour la fonction draw de Group

--- Paint/src/paint/elements/components/Group.java
+++ Paint/src/paint/elements/components/Group.java
@@ -5,26 +5,25 @@ import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Group extends Component {
-    
     private List<Component> compound = new ArrayList<>();
-    
+
     public Group(int posX, int posY) {
         super(posX, posY);
-        
+
     }
-    
-    public void addToCompound(Component c)
-    {
+
+    public void addToCompound(Component c) {
         compound.add(c);
     }
     
+    public void removeFromCompound(Component c) {
+        compound.remove(c);
+    }
+
     @Override
     public void draw(GraphicsContext g) {
-
-        for (Component c : compound)
-        {
+        compound.forEach((c) -> {
             c.draw(g);
-        }
+        });
     }
-    
 }

327ad8800cc45f307850305cea1bc4f129bdc75b
bilombardy <bilombardy@b20-73.dpt_info.iut.local>
2017-01-05 08:50:06 +0100

    Gestion des canvas pour les tabs

--- Paint/src/paint/elements/components/Oval.java
+++ Paint/src/paint/elements/components/Oval.java
@@ -8,6 +8,7 @@ import javafx.scene.canvas.GraphicsContext;
  * @author bilombardy
  */
 public class Oval extends Component {
+
     private int width;
     private int height;
 
@@ -16,21 +17,23 @@ public class Oval extends Component {
         setWidth(width);
         setHeight(height);
     }
-    
+
     private void setWidth(int width) {
-        if (width < 1)
+        if (width < 1) {
             width = 1;
-        
+        }
+
         this.width = width;
     }
-    
+
     private void setHeight(int height) {
-        if (height < 1)
+        if (height < 1) {
             height = 1;
-        
+        }
+
         this.height = height;
     }
-    
+
     @Override
     public void draw(GraphicsContext g) {
         g.setFill(color);

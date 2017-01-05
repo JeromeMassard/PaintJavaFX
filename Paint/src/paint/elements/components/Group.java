30c3d9dd5b2d3265099bca47839b7ed9e4cc0d7f
jemassard <jemassard@b20-74.dpt_info.iut.local>
2017-01-05 08:59:58 +0100

    Group implementation

--- Paint/src/paint/elements/components/Group.java
+++ Paint/src/paint/elements/components/Group.java
@@ -1,12 +1,30 @@
 package paint.elements.components;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.canvas.GraphicsContext;
+
 public class Group extends Component {
     
-    
+    private List<Component> compound = new ArrayList<>();
     
     public Group(int posX, int posY) {
         super(posX, posY);
         
+    }
+    
+    public void addToCompound(Component c)
+    {
+        compound.add(c);
+    }
+    
+    @Override
+    public void draw(GraphicsContext g) {
+
+        for (Component c : compound)
+        {
+            c.draw(g);
+        }
     }
     
 }
